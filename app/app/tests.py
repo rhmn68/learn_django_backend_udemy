ca9c24fcf52ee45bf12bfbf3c0b2eae2f866bb14
auliarahman <auliar68@gmail.com>
2025-09-01 06:32:04 +0700

    handle testing for db

---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTest(SimpleTestCase):
+    """Test the calc module."""
+    
+    def test_add_number(self):
+        """Test adding numbers together."""
+        res = calc.add(5, 6)
+        
+        self.assertEqual(res, 11)
+        
+    def test_substract_numbers(self):
+        """Test substracting number."""
+        res = calc.substract(10, 15)
+        
+        self.assertEqual(res, 5)
