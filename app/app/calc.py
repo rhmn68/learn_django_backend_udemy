24f46046bb004dff133bc4cdddb113fe0cdf3583
unknown <rhmn68>
2025-09-01 10:46:51 +0700

    fixing lint

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,11 +2,14 @@
 Calculator Functions
 """
 
+
 def add(x, y):
     """Add x and y and return result."""
+
     return x + y
 
 
 def substract(x, y):
     """Substract x from y and return result."""
+
     return y - x
