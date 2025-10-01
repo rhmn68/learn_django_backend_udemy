81da8c7e2e3c1083b61e823f1b5e774bd3c79387
auliarahman <auliar68@gmail.com>
2025-10-01 20:14:16 +0700

    latest_push

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,9 @@
+"""
+Tests for the Django admin modifications.
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+
